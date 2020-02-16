f2e833fc74966c25678ffbff66e521e649ec29ca
Bei Li <kylinroc@gmail.com>
2020-02-17 02:52:06 +0800

    Update Field

--- Sources/MastodonKit/Field.swift
+++ Sources/MastodonKit/Field.swift
@@ -1,1 +1,19 @@
-public struct Field: Codable {}
+import Foundation
+
+/// Represents a profile field as a name-value pair with optional verification.
+public struct Field: Codable {
+    /// The key of a given field's key-value pair.
+    ///
+    /// Added in 2.4.0
+    public var name: String
+
+    /// The value associated with the `name` key.
+    ///
+    /// Added in 2.4.0
+    public var value: String
+
+    /// Timestamp of when the server verified a URL value for a rel="me” link.
+    ///
+    /// Added in 2.6.0
+    public var verifiedAt: Date?
+}
