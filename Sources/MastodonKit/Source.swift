4850ed5deaa77aef86de8b609c2fa6dfcabac8f2
Bei Li <kylinroc@gmail.com>
2020-02-17 10:27:50 +0800

    Update Source

--- Sources/MastodonKit/Source.swift
+++ Sources/MastodonKit/Source.swift
@@ -1,1 +1,41 @@
-public struct Source: Codable {}
+/// Represents display or publishing preferences of user's own account.
+///
+/// Returned as an additional entity when verifying and updated credentials, as an attribute of `Account`.
+public struct Source: Codable {
+    public enum Privacy: String, Codable {
+        case `public`
+        case unlisted
+        case `private`
+        case direct
+    }
+
+    /// Profile bio.
+    ///
+    /// Added in 1.5.0
+    public var note: String
+
+    /// The default post privacy to be used for new statuses.
+    ///
+    /// Added in 1.5.0
+    public var privacy: Privacy
+
+    /// Whether new statuses should be marked sensitive by default.
+    ///
+    /// Added in 1.5.0
+    public var sensitive: Bool
+
+    /// Metadata about the account.
+    ///
+    /// Added in 2.4.0
+    public var fields: [Field]
+
+    /// The default posting language for new statuses.
+    ///
+    /// Added in 2.4.2
+    public var language: String?
+
+    /// The number of pending follow requests.
+    ///
+    /// Added in 3.0.0
+    public var followRequestsCount: Int
+}
