318f1325e83abff3e8fad42dc938fb1aad994e0c
Bei Li <kylinroc@gmail.com>
2020-02-19 00:49:06 +0800

    Update Card

--- Sources/MastodonKit/Entities/Card.swift
+++ Sources/MastodonKit/Entities/Card.swift
@@ -1,1 +1,103 @@
-public struct Card: Codable {}
+import Foundation
+
+/// Type of Card.
+public enum CardType: String, Codable {
+    case link
+    case photo
+    case video
+    case rich
+}
+
+/// Represents a rich preview card that is generated using OpenGraph tags from a URL.
+public struct Card: Codable {
+    /// Location of linked resource.
+    ///
+    /// Added in 1.0.0
+    public var url: URL
+
+    /// Title of linked resource.
+    ///
+    /// Added in 1.0.0
+    public var title: String
+
+    /// Description of preview.
+    ///
+    /// Added in 1.0.0
+    public var description: String
+
+    /// Preview thumbnail.
+    ///
+    /// Added in 1.0.0
+    public var image: URL?
+
+    /// The type of the preview card.
+    ///
+    /// Added in 1.3.0
+    public var type: CardType
+
+    /// The author of the original resource.
+    ///
+    /// Added in 1.3.0
+    public var authorName: String?
+
+    /// A link to the author of the original resource.
+    ///
+    /// Added in 1.3.0
+    public var authorURL: URL?
+
+    /// The provider of the original resource.
+    ///
+    /// Added in 1.3.0
+    public var providerName: String?
+
+    /// A link to the provider of the original resource.
+    ///
+    /// Added in 1.3.0
+    public var providerURL: URL?
+
+    /// HTML to be used for generating the preview card.
+    ///
+    /// Added in 1.3.0
+    public var html: String?
+
+    /// Width of preview, in pixels.
+    ///
+    /// Added in 1.3.0
+    public var width: Double?
+
+    /// Height of preview, in pixels.
+    ///
+    /// Added in 1.3.0
+    public var height: Double?
+
+    /// Used for photo embeds, instead of custom `html`.
+    ///
+    /// Added in 2.1.0
+    public var embedURL: URL? {
+        if let _embedURL = _embedURL {
+            return URL(string: _embedURL)
+        } else {
+            return nil
+        }
+    }
+
+    private var _embedURL: String?
+}
+
+extension Card {
+    private enum CodingKeys: String, CodingKey {
+        case url
+        case title
+        case description
+        case image
+        case type
+        case authorName = "author_name"
+        case authorURL = "author_url"
+        case providerName = "provider_name"
+        case providerURL = "provider_url"
+        case html
+        case width
+        case height
+        case _embedURL = "embed_url"
+    }
+}
---
+++ Tests/MastodonKitTests/CardTests.swift
@@ -0,0 +1,40 @@
+import MastodonKit
+import XCTest
+
+final class CardTests: XCTestCase {
+    func testDecoding() {
+        let data = #"""
+            {
+              "url": "https://www.youtube.com/watch?v=OMv_EPMED8Y",
+              "title": "♪ Brand New Friend (Christmas Song!)",
+              "description": "",
+              "type": "video",
+              "author_name": "YOGSCAST Lewis & Simon",
+              "author_url": "https://www.youtube.com/user/BlueXephos",
+              "provider_name": "YouTube",
+              "provider_url": "https://www.youtube.com/",
+              "html": "<iframe width=\"480\" height=\"270\" src=\"https://www.youtube.com/embed/OMv_EPMED8Y?feature=oembed\" frameborder=\"0\" allowfullscreen=\"\"></iframe>",
+              "width": 480,
+              "height": 270,
+              "image": "https://files.mastodon.social/preview_cards/images/014/179/145/original/9cf4b7cf5567b569.jpeg",
+              "embed_url": ""
+            }
+            """#
+            .data(using: .utf8)!
+
+        let card = try! JSONDecoder().decode(Card.self, from: data)
+        XCTAssertEqual(card.url, URL(string: "https://www.youtube.com/watch?v=OMv_EPMED8Y"))
+        XCTAssertEqual(card.title, "♪ Brand New Friend (Christmas Song!)")
+        XCTAssertEqual(card.description, "")
+        XCTAssertEqual(card.image, URL(string: "https://files.mastodon.social/preview_cards/images/014/179/145/original/9cf4b7cf5567b569.jpeg"))
+        XCTAssertEqual(card.type, .video)
+        XCTAssertEqual(card.authorName, "YOGSCAST Lewis & Simon")
+        XCTAssertEqual(card.authorURL, URL(string: "https://www.youtube.com/user/BlueXephos"))
+        XCTAssertEqual(card.providerName, "YouTube")
+        XCTAssertEqual(card.providerURL, URL(string: "https://www.youtube.com/"))
+        XCTAssertEqual(card.html, "<iframe width=\"480\" height=\"270\" src=\"https://www.youtube.com/embed/OMv_EPMED8Y?feature=oembed\" frameborder=\"0\" allowfullscreen=\"\"></iframe>")
+        XCTAssertEqual(card.width, 480)
+        XCTAssertEqual(card.height, 270)
+        XCTAssertEqual(card.embedURL, nil)
+    }
+}
