4e6d23222e0314b66070dad5da62e8a7d4c2e922
Bei Li <kylinroc@gmail.com>
2020-02-22 02:51:11 +0800

    Update Tag

--- Sources/MastodonKit/Entities/Tag.swift
+++ Sources/MastodonKit/Entities/Tag.swift
@@ -1,1 +1,21 @@
-public struct Tag: Codable {}
+import Foundation
+
+/// Represents a hashtag used within the content of a status.
+public struct Tag: Codable {
+    /// The value of the hashtag after the # sign.
+    ///
+    /// Added in 0.9.0
+    public var name: String
+
+    /// A link to the hashtag on the instance.
+    ///
+    /// Added in 0.9.0
+    public var url: URL
+
+    /// Usage statistics for given days.
+    ///
+    /// Added in 2.4.1
+    public var history: [History] { _history ?? [] }
+
+    private var _history: [History]?
+}
