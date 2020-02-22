1867b9ef3e2efd53d828676e8b77e2067c568dcf
Bei Li <kylinroc@gmail.com>
2020-02-22 15:00:28 +0800

    Update Client

--- Sources/MastodonKit/Client.swift
+++ Sources/MastodonKit/Client.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
 public final class Client {
     public let baseURL: URL
 
@@ -8,42 +13,52 @@ public final class Client {
     }
 
     public func send<T>(request: Request<T>, completion: @escaping (Result<T, Error>) -> Void) {
-        let url = baseURL.appendingPathComponent(request.path)
-        var urlRequest = URLRequest(url: url)
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.httpMethod = request.method
-        do {
-            urlRequest.httpBody = try JSONEncoder().encode(request.parameters)
-
-            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-                if let error = error {
-                    completion(.failure(error))
-                    return
-                }
-
-                guard let data = data else {
-                    return
-                }
-
-                do {
-                    let decoder = Self.makeJSONDecoder()
-                    decoder.dateDecodingStrategy = .formatted(Self.dateFormatter)
-                    let value = try decoder.decode(T.self, from: data)
-                    completion(.success(value))
-                } catch {
-                    completion(.failure(error))
-                }
-            }.resume()
-        } catch {
-            completion(.failure(error))
+        var urlRequest: URLRequest
+        switch request.httpMethod {
+        case .get:
+            var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)!
+            urlComponents.path = request.path
+            urlComponents.queryItems = request.parameters.map(URLQueryItem.init)
+            urlRequest = URLRequest(url: urlComponents.url!)
+        case .post:
+            let url = baseURL.appendingPathComponent(request.path)
+            urlRequest = URLRequest(url: url)
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.httpMethod = request.httpMethod.rawValue
+            do {
+                urlRequest.httpBody = try JSONEncoder().encode(request.parameters)
+            } catch {
+                completion(.failure(error))
+                return
+            }
         }
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                return
+            }
+
+            do {
+                let decoder = Self.makeJSONDecoder()
+                decoder.dateDecodingStrategy = .formatted(Self.dateFormatter)
+                let value = try decoder.decode(T.self, from: data)
+                completion(.success(value))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
 
 extension Client {
     public struct Request<T: Decodable> {
         var path: String
-        var method: String
+        var httpMethod: HTTPMethod
         var parameters: [String: String]
     }
 }
@@ -53,9 +68,21 @@ extension Client.Request {
         clientName: String,
         redirectURIs: String = "urn:ietf:wg:oauth:2.0:oob"
     ) -> Client.Request<Application> {
-        .init(path: "/api/v1/apps", method: "POST", parameters: [
+        .init(path: "/api/v1/apps", httpMethod: .post, parameters: [
             "client_name": clientName,
             "redirect_uris": redirectURIs,
+        ])
+    }
+
+    public static func fetchPublicTimeline(
+        local: Bool = false,
+        onlyMedia: Bool = false,
+        limit: Int = 20
+    ) -> Client.Request<[Status]> {
+        .init(path: "/api/v1/timelines/public", httpMethod: .get, parameters: [
+            "local": "\(local)",
+            "only_media": "\(onlyMedia)",
+            "limit": "\(limit)",
         ])
     }
 }
