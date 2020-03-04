1e87118b98e6826925787472df06dcc852ded73d
Bei Li <kylinroc@gmail.com>
2020-03-04 12:15:38 +0800

    Rename Instance to Server

--- Sources/MastodonKit/Server.swift
+++ Sources/MastodonKit/Server.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Represents the software instance of Mastodon running on this domain.
-public struct Instance: Codable {
+public struct Server: Codable {
     /// The domain name of the instance.
     ///
     /// Added in 1.1.0
