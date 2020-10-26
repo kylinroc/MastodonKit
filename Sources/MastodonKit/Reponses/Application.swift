ccbd67ec8e4c11e890443c5850cce975b7b1db29
Bei Li <kylinroc@gmail.com>
2020-10-26 21:45:33 +0800

    Update

--- Sources/MastodonKit/Reponses/Application.swift
+++ Sources/MastodonKit/Reponses/Application.swift
@@ -1,43 +1,22 @@
 import Foundation
 
 extension Responses {
-    /// Represents an application that interfaces with the REST API to access accounts or post toots.
     public struct Application: Codable {
-        private enum CodingKeys: String, CodingKey {
-            case name
-            case website
-            case clientID = "client_id"
-            case clientSecret = "client_secret"
-            case vapidKey = "vapid_key"
-            case redirectURI = "redirect_uri"
-        }
-
-        /// The name of your application.
-        ///
-        /// Added in 0.9.9
-        public let name: String
-
-        /// The website associated with your application.
-        ///
-        /// Added in 0.9.9
-        public let website: String?
-
-        /// Client ID key, to be used for obtaining OAuth tokens.
-        ///
-        /// Added in 0.9.9
         public let clientID: String?
-
-        /// Client secret key, to be used for obtaining OAuth tokens.
-        ///
-        /// Added in 0.9.9
         public let clientSecret: String?
-
-        /// Used for Push Streaming API.
-        ///
-        /// Added in 2.8.0
-        public let vapidKey: String?
-
+        public let name: String
         public let redirectURI: String?
+        public let website: String?
+    }
+}
+
+extension Responses.Application {
+    private enum CodingKeys: String, CodingKey {
+        case clientID = "client_id"
+        case clientSecret = "client_secret"
+        case name
+        case redirectURI = "redirect_uri"
+        case website
     }
 }
 
