e532bff392174177ff18ec074f16a769444694ff
Bei Li <kylinroc@gmail.com>
2020-11-02 22:03:34 +0800

    Update

--- Sources/MastodonKit/Responses/Application.swift
+++ Sources/MastodonKit/Responses/Application.swift
@@ -1,23 +1,23 @@
 extension Responses {
-    public struct Application: Decodable {
+    public struct Application {
+        public let name: String
+        public let website: String?
+        public let id: String
         public let clientID: String?
         public let clientSecret: String?
-        public let id: String
-        public let name: String
         public let redirectURI: String
         public let vapidKey: String?
-        public let website: String?
     }
 }
 
-extension Responses.Application {
+extension Responses.Application: Decodable {
     private enum CodingKeys: String, CodingKey {
+        case name
+        case website
+        case id
         case clientID = "client_id"
         case clientSecret = "client_secret"
-        case id
-        case name
         case redirectURI = "redirect_uri"
         case vapidKey = "vapid_key"
-        case website
     }
 }
