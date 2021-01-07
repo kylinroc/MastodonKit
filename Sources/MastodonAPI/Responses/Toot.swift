bac037ba3cf6763c8dacc8e0f442a6002134ad3e
Bei Li <kylinroc@gmail.com>
2021-01-08 00:21:40 +0800

    Update

--- Sources/MastodonAPI/Responses/Toot.swift
+++ Sources/MastodonAPI/Responses/Toot.swift
@@ -3,11 +3,13 @@ extension Responses {
         private enum CodingKeys: String, CodingKey {
             case id
             case account
+            case content
             case boostedToot = "reblog"
         }
 
         public let id: String
         public let account: Account
+        public let content: String
         public let boostedToot: Toot?
     }
 }
