1247003c47d1934284e3f2a92d2e39a1b5c6ec22
Bei Li <kylinroc@gmail.com>
2021-02-09 21:16:10 +0800

    Update Account

--- Sources/MastodonAPI/Responses/Account.swift
+++ Sources/MastodonAPI/Responses/Account.swift
@@ -2,6 +2,8 @@ import Foundation
 
 extension Responses {
     public struct Account {
+        public let id: String
+        public let username: String
         public let fullUsername: String
         public let displayName: String
         public let staticAvatarURL: URL
@@ -10,8 +12,23 @@ extension Responses {
 
 extension Responses.Account: Decodable {
     private enum CodingKeys: String, CodingKey {
+        case id
+        case username
         case fullUsername = "acct"
         case displayName = "display_name"
         case staticAvatarURL = "avatar_static"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        username = try container.decode(String.self, forKey: .username)
+        fullUsername = try container.decode(String.self, forKey: .fullUsername)
+        if let displayName = try container.decodeIfPresent(String.self, forKey: .displayName), !displayName.isEmpty {
+            self.displayName = displayName
+        } else {
+            self.displayName = username
+        }
+        staticAvatarURL = try container.decode(URL.self, forKey: .staticAvatarURL)
     }
 }
