4bfd2fee6141b7dcd51dbdb794ce5ab959fcd729
Bei Li <kylinroc@gmail.com>
2021-02-10 11:34:58 +0800

    Update

--- Sources/MastodonAPI/Responses/Toot.swift
+++ Sources/MastodonAPI/Responses/Toot.swift
@@ -8,6 +8,9 @@ extension Responses {
         public let attachments: [Attachment]
         public let creationDate: Date
         public let visibility: Visibility
+        public let repliesCount: Int
+        public let boostsCount: Int
+        public let favoritesCount: Int
         public let boostedToot: Toot?
 
         private enum CodingKeys: String, CodingKey {
@@ -17,6 +20,9 @@ extension Responses {
             case attachments = "media_attachments"
             case creationDate = "created_at"
             case visibility
+            case repliesCount = "replies_count"
+            case boostsCount = "reblogs_count"
+            case favoritesCount = "favourites_count"
             case boostedToot = "reblog"
         }
     }
