801a63d20024e4deb9ea0b812cfdc620649fe4d0
Bei Li <kylinroc@gmail.com>
2021-02-13 00:15:17 +0800

    Update Toot

--- Sources/MastodonAPI/Responses/Toot.swift
+++ Sources/MastodonAPI/Responses/Toot.swift
@@ -8,6 +8,9 @@ extension Responses {
         public let attachments: [Attachment]
         public let creationDate: Date
         public let visibility: Visibility
+        public let isBoosted: Bool
+        public let isFavorited: Bool
+        public let isBookmarked: Bool
         public let repliesCount: Int
         public let boostsCount: Int
         public let favoritesCount: Int
@@ -20,6 +23,9 @@ extension Responses {
             case attachments = "media_attachments"
             case creationDate = "created_at"
             case visibility
+            case isBoosted = "reblogged"
+            case isFavorited = "favourited"
+            case isBookmarked = "bookmarked"
             case repliesCount = "replies_count"
             case boostsCount = "reblogs_count"
             case favoritesCount = "favourites_count"
