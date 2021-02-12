2166a886cfbe1b8d5ee4171168fcda33cf394ffd
Bei Li <kylinroc@gmail.com>
2021-02-12 14:17:56 +0800

    Update Notification

--- Sources/MastodonAPI/Responses/Notification.swift
+++ Sources/MastodonAPI/Responses/Notification.swift
@@ -1,10 +1,22 @@
 extension Responses {
+    public enum NotificationType: String, Decodable {
+        case follow
+        case followRequest = "follow_request"
+        case mention
+        case boost = "reblog"
+        case favorite = "favourite"
+        case poll
+        case toot = "status"
+    }
+
     public struct Notification: Decodable {
         public let id: String
+        public let type: NotificationType
         public let toot: Toot?
 
         private enum CodingKeys: String, CodingKey {
             case id
+            case type
             case toot = "status"
         }
     }
