fd7cc95344bf5972d7f1fdc286c3700369a60a93
Bei Li <kylinroc@gmail.com>
2021-02-19 23:50:49 +0800

    Update

--- Sources/MastodonAPI/Requests/API.V1.Statuses.swift
+++ Sources/MastodonAPI/Requests/API.V1.Statuses.swift
@@ -6,8 +6,28 @@ extension Requests.API.V1 {
             self.id = id
         }
 
+        public func reblog() -> Request<Responses.Toot> {
+            Request(path: "/api/v1/statuses/\(id)/reblog", httpMethod: .post(nil))
+        }
+
+        public func unreblog() -> Request<Responses.Toot> {
+            Request(path: "/api/v1/statuses/\(id)/unreblog", httpMethod: .post(nil))
+        }
+
         public func favourite() -> Request<Responses.Toot> {
             Request(path: "/api/v1/statuses/\(id)/favourite", httpMethod: .post(nil))
+        }
+
+        public func unfavourite() -> Request<Responses.Toot> {
+            Request(path: "/api/v1/statuses/\(id)/unfavourite", httpMethod: .post(nil))
+        }
+
+        public func bookmark() -> Request<Responses.Toot> {
+            Request(path: "/api/v1/statuses/\(id)/bookmark", httpMethod: .post(nil))
+        }
+
+        public func unbookmark() -> Request<Responses.Toot> {
+            Request(path: "/api/v1/statuses/\(id)/unbookmark", httpMethod: .post(nil))
         }
     }
 }
