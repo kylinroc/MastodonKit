f0c297a84379352bd4e992298417f6ecc2a22631
Bei Li <kylinroc@gmail.com>
2021-02-23 22:09:52 +0800

    Update

--- Sources/MastodonAPI/Responses/Account.swift
+++ Sources/MastodonAPI/Responses/Account.swift
@@ -2,26 +2,51 @@ import Foundation
 
 extension Responses {
     public struct Account: Decodable {
+        // 0.1.0
         public let id: String
+
+        // 0.1.0
         public let username: String
+
+        // 0.1.0
         public let fullUsername: String
+
+        // 0.1.0
         public let displayName: String
-        public let staticAvatarURL: URL
-        public let staticHeaderURL: URL
+
+        // 0.1.0
+        public let note: String
+
+        // 0.1.0
         public let statusesCount: Int
+
+        // 0.1.0
         public let followingCount: Int
+
+        // 0.1.0
         public let followersCount: Int
+
+        // 1.1.2
+        public let staticAvatarURL: URL
+
+        // 1.1.2
+        public let staticHeaderURL: URL
+
+        // 2.4.0
+        public let fields: [Field]
 
         private enum CodingKeys: String, CodingKey {
             case id
             case username
             case fullUsername = "acct"
             case displayName = "display_name"
-            case staticAvatarURL = "avatar_static"
-            case staticHeaderURL = "header_static"
+            case note
             case statusesCount = "statuses_count"
             case followingCount = "following_count"
             case followersCount = "followers_count"
+            case staticAvatarURL = "avatar_static"
+            case staticHeaderURL = "header_static"
+            case fields
         }
     }
 }
---
+++ Sources/MastodonAPI/Responses/Field.swift
@@ -0,0 +1,9 @@
+extension Responses {
+    public struct Field: Decodable, Hashable {
+        // 2.4.0
+        public let name: String
+
+        // 2.4.0
+        public let value: String
+    }
+}
