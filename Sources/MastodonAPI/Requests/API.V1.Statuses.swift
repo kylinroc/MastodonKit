6eb93d2cbc51f8088bee4193e1dbee51d0015924
Bei Li <kylinroc@gmail.com>
2021-03-16 23:18:23 +0800

    Update

--- Sources/MastodonAPI/Requests/API.V1.Statuses.swift
+++ Sources/MastodonAPI/Requests/API.V1.Statuses.swift
@@ -1,3 +1,6 @@
+import Foundation
+import HTTPLinkHeader
+
 extension Requests.API.V1 {
     public struct Statuses {
         private let id: String
@@ -35,5 +38,27 @@ extension Requests.API.V1.Statuses {
 
     public func unbookmark() -> Request<Responses.Status> {
         Request(path: "/api/v1/statuses/\(id)/unbookmark", httpMethod: .post(nil))
+    }
+
+    public func rebloggedBy(pagination: HTTPLinkHeader? = nil) -> Request<Paged<[Responses.Account]>> {
+        let queryItems: [URLQueryItem]?
+        if let pagination = pagination, let urlComponents = URLComponents(string: pagination.uriReference) {
+            queryItems = urlComponents.queryItems
+        } else {
+            queryItems = nil
+        }
+
+        return Request(path: "/api/v1/statuses/\(id)/reblogged_by", httpMethod: .get(queryItems))
+    }
+
+    public func favouritedBy(pagination: HTTPLinkHeader? = nil) -> Request<Paged<[Responses.Account]>> {
+        let queryItems: [URLQueryItem]?
+        if let pagination = pagination, let urlComponents = URLComponents(string: pagination.uriReference) {
+            queryItems = urlComponents.queryItems
+        } else {
+            queryItems = nil
+        }
+
+        return Request(path: "/api/v1/statuses/\(id)/favourited_by", httpMethod: .get(queryItems))
     }
 }
