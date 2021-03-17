d1a8ba7c37d53fa1721ca8a35df8d5e8d2693c39
Bei Li <kylinroc@gmail.com>
2021-03-17 21:59:20 +0800

    Update Attachment

--- Sources/MastodonAPI/Responses/Attachment.swift
+++ Sources/MastodonAPI/Responses/Attachment.swift
@@ -1,13 +1,59 @@
 import Foundation
 
 extension Responses {
+    public enum AttachmentType: String, Decodable {
+        // 0.6.0
+        case unknown
+        // 0.6.0
+        case image
+        // 0.6.0
+        case gifv
+        // 0.6.0
+        case video
+        // 2.9.1
+        case audio
+    }
+
+    public struct AttachmentMetadata: Decodable {}
+
     public struct Attachment: Decodable {
+        // 0.6.0
         public let id: String
+
+        // 0.6.0
+        public let type: AttachmentType
+
+        // 0.6.0
+        public let url: URL
+
+        // 0.6.0
         public let previewURL: URL?
+
+        // 0.6.0
+        public let remoteURL: URL?
+
+        // 0.6.0
+        public let textURL: URL?
+
+        // 1.5.0
+        public let metadata: AttachmentMetadata?
+
+        // 2.0.0
+        public let description: String?
+
+        // 2.8.1
+        public let blurhash: String?
 
         private enum CodingKeys: String, CodingKey {
             case id
+            case type
+            case url
             case previewURL = "preview_url"
+            case remoteURL = "remote_url"
+            case textURL = "text_url"
+            case metadata = "meta"
+            case description
+            case blurhash
         }
     }
 }
