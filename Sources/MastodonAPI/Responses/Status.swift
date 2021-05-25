3ce83cb11a1e03cc59df9211e1d30fa8a44f21fd
Bei Li <kylinroc@gmail.com>
2021-05-25 18:40:21 +0800

    Update Status

--- Sources/MastodonAPI/Responses/Status.swift
+++ Sources/MastodonAPI/Responses/Status.swift
@@ -68,6 +68,8 @@ extension Responses {
         // 3.1.0
         public let isBookmarked: Bool
 
+        // MARK: Decodable
+
         private enum CodingKeys: String, CodingKey {
             case id
             case account
@@ -91,6 +93,44 @@ extension Responses {
             case repliesCount = "replies_count"
             case poll
             case isBookmarked = "bookmarked"
+        }
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            id = try container.decode(String.self, forKey: .id)
+            account = try container.decode(Account.self, forKey: .account)
+            contentWarning = try container.decode(String.self, forKey: .contentWarning)
+            content = try container.decode(String.self, forKey: .content)
+            creationDate = try container.decode(Date.self, forKey: .creationDate)
+            isReblogged = try container.decode(Bool.self, forKey: .isReblogged)
+            isFavorited = try container.decode(Bool.self, forKey: .isFavorited)
+            reblogsCount = try container.decode(Int.self, forKey: .reblogsCount)
+            favoritesCount = try container.decode(Int.self, forKey: .favoritesCount)
+            rebloggedStatus = try container.decodeIfPresent(Status.self, forKey: .rebloggedStatus)
+            inReplyToID = try container.decodeIfPresent(String.self, forKey: .inReplyToID)
+
+            if let string = try container.decodeIfPresent(String.self, forKey: .url) {
+                if let url = URL(string: string) {
+                    self.url = url
+                } else if let string = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                    url = URL(string: string)
+                } else {
+                    url = nil
+                }
+            } else {
+                url = nil
+            }
+
+            attachments = try container.decode([Attachment].self, forKey: .attachments)
+            mentions = try container.decode([Mention].self, forKey: .mentions)
+            tags = try container.decode([Tag].self, forKey: .tags)
+            isSensitive = try container.decode(Bool.self, forKey: .isSensitive)
+            visibility = try container.decode(Visibility.self, forKey: .visibility)
+            pinned = try container.decodeIfPresent(Bool.self, forKey: .pinned)
+            emojis = try container.decode([Emoji].self, forKey: .emojis)
+            repliesCount = try container.decode(Int.self, forKey: .repliesCount)
+            poll = try container.decodeIfPresent(Poll.self, forKey: .poll)
+            isBookmarked = try container.decode(Bool.self, forKey: .isBookmarked)
         }
     }
 }
